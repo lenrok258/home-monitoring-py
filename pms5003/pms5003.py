6e277c2a756f68a3fd19f465a3b896b881c88fad
kornel.zemla <kornel.zemla@ocado.com>
2017-02-10 14:32:20 +0100

    Putting pms sensor to sleep every few seconds

--- pms5003/pms5003.py
+++ pms5003/pms5003.py
@@ -9,62 +9,50 @@ else:
     import mocks.GPIO_mock as GPIO
     import mocks.serial_mock as serial
 
-from pms5003_data import PMS5003Data
+import pms5003_data
 
 
 class PMS5003:
     __SET_GPIO_PIN = 0
-    __SLEEP_DURATION_S = 1 * 5
-    __ACTIVE_DURATION = 1 * 1
+    __SLEEP_DURATION_S = config['pms5003']['sleep-duration-sec']
+    __ACTIVE_DURATION_S = config['pms5003']['active-duration-sec']
 
-    port = None
-    prev_data = None
-    enabled = None
+    __port = None
+    __prev_data = None
+    __sleeping = None
 
     def __init__(self):
-        self.wake_up()
+        self.__wake_up()
 
     def __enter__(self):
-        self.port = self.__open_port()
+        self.__port = self.__open_port()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         print "Closing serial port..."
-        if self.port is not None:
-            self.port.close()
+        if self.__port is not None:
+            self.__port.close()
         print "Port closed"
 
     def read_data(self, clock_tick):
         self.__swith_on_off(clock_tick)
-        data = self.__read_pm_line()
-        self.port.flushInput()
-        self.prev_data = data
-        return data
 
-    def __read_pm_line(self):
-        rv = b''
-        while True:
-            ch1 = self.port.read()
-            if ch1 == b'\x42':
-                ch2 = self.port.read()
-                if ch2 == b'\x4d':
-                    rv += ch1 + ch2
-                    rv += self.port.read(30)
-                    line = rv[4:32]
-                    data = unpack('>hhhhhhhhhhhhhh', line)
-                    return PMS5003Data(data)
+        if self.__sleeping:
+            return self.__prev_data
+        else:
+            return self.__read_data_from_sensor()
 
-    def put_to_sleep(self):
+    def __put_to_sleep(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__SET_GPIO_PIN, GPIO.OUT)
         GPIO.output(self.__SET_GPIO_PIN, GPIO.LOW)
-        self.enabled = False
+        self.__sleeping = False
 
-    def wake_up(self):
+    def __wake_up(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__SET_GPIO_PIN, GPIO.OUT)
         GPIO.output(self.__SET_GPIO_PIN, GPIO.HIGH)
-        self.enabled = True
+        self.__sleeping = True
 
     def __open_port(self):
         print "Opening Serial Port...",
@@ -73,9 +61,29 @@ class PMS5003:
         return port
 
     def __swith_on_off(self, clock_tick):
-        if self.enabled and (clock_tick % self.__ACTIVE_DURATION == 0):
-            print "Putting to sleep {}, {}, {}".format(clock_tick, self.enabled, clock_tick % self.__ACTIVE_DURATION)
-            self.put_to_sleep()
-        if not self.enabled and (clock_tick % self.__SLEEP_DURATION_S == 0):
-            print "Waking up {}, {}, {}".format(clock_tick, self.enabled, clock_tick % self.__SLEEP_DURATION_S)
-        self.wake_up()
+        cycle_duration = self.__SLEEP_DURATION_S + self.__ACTIVE_DURATION_S
+        if clock_tick % cycle_duration == 0:
+            print "Waking up. Clock tick: {}".format(clock_tick)
+            self.__wake_up()
+        elif clock_tick % cycle_duration == self.__ACTIVE_DURATION_S:
+            print "Putting to sleep. Clock tick: {}".format(clock_tick)
+            self.__wake_up()
+
+    def __read_data_from_sensor(self):
+        data = self.__read_pm_line()
+        self.__port.flushInput()
+        self.__prev_data = data
+        return data
+
+    def __read_pm_line(self):
+        rv = b''
+        while True:
+            ch1 = self.__port.read()
+            if ch1 == b'\x42':
+                ch2 = self.__port.read()
+                if ch2 == b'\x4d':
+                    rv += ch1 + ch2
+                    rv += self.__port.read(30)
+                    line = rv[4:32]
+                    data = unpack('>hhhhhhhhhhhhhh', line)
+                    return pms5003_data.PMS5003Data(data)
