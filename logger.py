3977ddf827dbc2f0cd702f551ac736197b1f2492
Kornel Zemla <kornelzemla@gmail.com>
2017-10-11 20:11:18 +0200

    Temprary logger

---
+++ logger.py
@@ -0,0 +1,10 @@
+class Logger:
+    def __init__(self, tag):
+        pass;
+    
+    def info(self, m):
+        print m
+    def error(self, m):
+        print m
+    def warn(self, m):
+        print m
