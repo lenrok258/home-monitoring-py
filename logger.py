2aa70368ea17d36bce67b386ef9e2159513ce74c
kornel.zemla <kornel.zemla@ocado.com>
2017-10-12 10:00:39 +0200

    Logger

--- logger.py
+++ logger.py
@@ -1,10 +1,23 @@
+from datetime import datetime
+
+
 class Logger:
-    def __init__(self, tag):
-        pass;
-    
-    def info(self, m):
-        print m
-    def error(self, m):
-        print m
-    def warn(self, m):
-        print m
+
+    def __init__(self, tag_name):
+        self.tag_name = tag_name
+
+    def info(self, message):
+        self.__print('INFO', message)
+
+    def error(self, message):
+        self.__print('ERROR', message)
+
+    def warn(self, message):
+        self.__print('WARN', message)
+
+    def __print(self, level, message):
+        nowString = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print '{0} [{1}] {2}: {3}'.format(nowString,
+                                        level,
+                                        self.tag_name,
+                                        message)
